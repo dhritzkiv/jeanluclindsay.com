93b0411774343470082e7a996d983e65b68ba3ab
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2016-05-05 01:44:29 -0400

    renaming of variables; load static files from content folder

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const fs = require("fs");
 const express = require("express");
 
 const seriesRouter = require(path.join(process.cwd(), "routes", "series"));
+
+const publicFilesDirectory = path.join(process.cwd(), 'public');
+const seriesFilesDirectory = path.join(process.cwd(), "content", "series");
+
 const packageInfo = require(path.join(process.cwd(), "package.json"));
-const publicFileDirectory = path.join(process.cwd(), 'public');
-const indexFileContents = fs.readFileSync(path.join(publicFileDirectory, "index.html"), "utf8");
+const config = require(path.join(process.cwd(), "config.json"));
+const indexFileContents = fs.readFileSync(path.join(publicFilesDirectory, "index.html"), "utf8");
 
 /*const config = (env => {
 	let configPath = path.join(process.cwd(), "config.json");
@@ -19,8 +23,6 @@ const indexFileContents = fs.readFileSync(path.join(publicFileDirectory, "index.
 	return require(configPath);
 })(process.env.NODE_ENV);*/
 
-const config = require(path.join(process.cwd(), "config.json"));
-
 const app = express();
 
 app.set("title", packageInfo.name);
@@ -31,7 +33,7 @@ app.set('json spaces', '\t');
 /* Disable X-Powered-by header */
 app.disable('x-powered-by');
 
-app.use(express.static(publicFileDirectory, {
+app.use(express.static(publicFilesDirectory, {
 	maxAge: 1000 * 60 * 60 * 24 * 7
 }));
 
@@ -40,11 +42,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app
-.route("*")
-.get((req, res, next) => {
+app.get("*", (req, res, next) => {
 	
-	if (req.accepts("html", "json") === "json") {
+	if (/\.[a-z0-9]{2,4}/i.test(req.url) || req.accepts("html", "json") === "json") {
 		return next();
 	}
 	
@@ -56,5 +56,8 @@ app.get("/series", seriesRouter.getSeriesModels);
 app.get("/series/:slug", seriesRouter.findASeries, seriesRouter.getASeries);
 app.get("/series/:slug/pieces", seriesRouter.findASeries, seriesRouter.findASeriesPieces, seriesRouter.getASeriesPieces);
 
+app.use("/series", express.static(seriesFilesDirectory, {
+	maxAge: 1000 * 60 * 60 * 24
+}));
 
 app.listen(app.get("port"), () => console.log(`${new Date().toISOString()}: Server for ${app.get('title')} v.${app.get('version')} ${app.settings.env}, listening on port ${app.get('port')}`));
