ed54db7f30226e1a9ea5e5d6b810fd67b6ba1d86
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2016-05-26 00:45:32 -0400

    Config tweaks

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const environment = process.env.NODE_ENV || "development";
+const port = 2039;
 
 const config = {
 	environment,
-	port: 2039,
-	hostname: "http://localhost:2039",
+	port,
+	hostname: `http://localhost:${port}`,
 	sentry_dsn_client: "https://a3dba8a45490482fb632951b6eb60cc2@app.getsentry.com/78389"
 };
 
